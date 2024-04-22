143ae6309bb25b7897a17c09e7cd65c7c7001a70
lebarreroc <62678568+lebarreroc@users.noreply.github.com>
2024-04-22 11:16:11 -0500

    Ejercicios, ciclo for java

---
+++ ejercicios_2.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clase_java;
+
+/**
+ *
+ * @author Cesde
+ */
+public class ejercicios_2 {
+    public static void main(String[] args) {
+        for (int i = 100; i >= 10; i -= 2) {
+            System.out.println("la secuencia es " + i);
+        }
+    }
+}
---
+++ ejercicios_for_1.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clase_java;
+
+/**
+ * Name: LUIS EDUARDO BARRERO CÓRDOBA
+ * DATE :22/04/2024
+ * CLASE: ENFASIS EN JAVA
+ * CESDE
+ * DESCRIPTION: Este algoritmo permite realizar el calculo del área de un hexágono cuando
+ * el usuario introduce el área 
+ * 
+ *
+ * @author LB-PC
+ */
+public class ejercicios_for_1 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("la secuencia es " + i);
+        }
+    }
+}
+
